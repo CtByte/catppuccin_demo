caaa8e12f29a741fe582cb0063449024f59aa4cd
Csaba <georgelisabeth1231@gmail.com>
2024-12-22 17:15:24 +0100

    transparency demo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use eframe::egui::Align;
 use eframe::egui::CentralPanel;
 use eframe::egui::Context;
-use eframe::egui::Layout;
-use eframe::egui::Rounding;
 use eframe::egui::ViewportBuilder;
 
 pub struct App {}
@@ -19,38 +16,11 @@ impl eframe::App for App {
     }
 
     fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
-        catppuccin_egui::set_theme(ctx, catppuccin_egui::LATTE);
-
-        ctx.style_mut(|style| {
-            let rounding = Rounding::same(10.0);
-
-            style.visuals.widgets.noninteractive.rounding = rounding;
-            style.visuals.widgets.inactive.rounding = rounding;
-            style.visuals.widgets.hovered.rounding = rounding;
-            style.visuals.widgets.active.rounding = rounding;
-            style.visuals.widgets.open.rounding = rounding;
-        });
-
-        CentralPanel::default().frame(eframe::egui::Frame::none()).show(ctx, |ui| {
-            ui.horizontal_centered(|ui| {
-                ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
-                    for item in [
-                        "First".to_string(),
-                        "Second".to_string(),
-                        "Third".to_string(),
-                    ] {
-                        if ui
-                            .add(eframe::egui::SelectableLabel::new(
-                                "First" == item.to_string(),
-                                item.to_string(),
-                            ))
-                            .on_hover_text(<String as Into<String>>::into(item.clone()))
-                            .clicked()
-                        {};
-                    }
-                })
+        CentralPanel::default()
+            .frame(eframe::egui::Frame::none())
+            .show(ctx, |ui| {
+                ui.label("The background should be transparent");
             });
-        });
     }
 }
 
